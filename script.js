a686ca45b693f1565da841b5dd70d249b58d2082
Glitch (hello-webpage) <none>
2020-01-28 12:37:34 +0000

    👳🤗 Checkpoint ./devs/e.png:9099011/9 ./devs/diary.txt:9099011/13 ./script.js:9099011/393

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Code for the console and cookies. The cookies in here do not track and are made for fun. For info about
+// tracking cookies on our site, please visit our privacy policy at dhhsnews.glitch.me/privacy.html
 console.log("This is the console. Welcome!");
 document.cookie = "testcookie=test";
 document.cookie = "Riverside Rocks=This is a test cookie";
+document.cookie = "Streamer[358]=ha streamer 358 big funny meme"
+document.cookie = "secrets=Sometime i"
